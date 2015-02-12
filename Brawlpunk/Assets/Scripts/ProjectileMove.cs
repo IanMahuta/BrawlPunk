aaa83e5c4b65ac958aeed6d1791a7b15d945b977
Obliman <nato5342@hotmail.com>
2015-02-11 20:29:59 -0600

    Spread File
    Git didn't want to save this one for some reason.
    Just the ProjectileMove file alterations to add spread.

--- Brawlpunk/Assets/Scripts/ProjectileMove.cs
+++ Brawlpunk/Assets/Scripts/ProjectileMove.cs
@@ -6,7 +6,7 @@ public class ProjectileMove : MonoBehaviour {
 	// Bullets are physics-based and will move in their initial direction with a force specified by the gun that spawned them.
 	
 	void Start () { 
-		transform.rotation = Quaternion.AngleAxis(GunTransforms.angle*Mathf.Rad2Deg, Vector3.forward);
+		transform.rotation = Quaternion.AngleAxis((GunTransforms.angle+(Random.Range(-PlayerShoot.spread,PlayerShoot.spread))*Mathf.Rad2Deg, Vector3.forward);
 		transform.rigidbody2D.AddForce(transform.right*PlayerShoot.shotForce);
 	}
 
